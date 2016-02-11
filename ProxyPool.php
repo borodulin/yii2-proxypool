56109ecba01c739197b3e8f14d6fdac9da91cd15
Andrey Borodulin <borodulin@gmail.com>
2016-02-11 11:55:54 +0600

    component bootstrap

--- ProxyPool.php
+++ ProxyPool.php
@@ -3,8 +3,10 @@
 namespace conquer\proxypool;
 
 use yii\base\Component;
+use conquer\proxypool\console\ProxyController;
+use conquer\proxypool\console\ScanController;
 
-class ProxyPool extends Component
+class ProxyPool extends Component implements \yii\base\BootstrapInterface
 {
     public $connectionTable = '{{%connection}}';
     
@@ -15,5 +17,20 @@ class ProxyPool extends Component
     public $maxErrors = 20;
     
     public $checkInterval = 21600;
+ 
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        if ($app instanceof \yii\console\Application) {
+            $app->controllerMap[$this->id] = [
+                    'class' => ProxyController::className(),
+            ];
+            $app->controllerMap[$this->id . '-scan'] = [
+                    'class' => ScanController::className(),
+            ];
+        }
+    }
     
 }
